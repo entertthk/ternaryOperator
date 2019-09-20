b5ace3fcdbec6a0dfec5ae3be3024ee168f9407b
Opilane TTHK <opilane@tthk.ee>
2019-09-20 17:59:25 +0300

    ternary

--- liigaasta/Program.cs
+++ liigaasta/Program.cs
@@ -9,6 +9,45 @@ namespace liigaasta
     {
         static void Main(string[] args)
         {
+            /*
+            int birthYear;
+            Console.WriteLine("What year did you born");
+            birthYear = Int32.Parse(Console.ReadLine());
+
+            
+            if (birthYear % 4 == 0)
+            {
+                Console.WriteLine($"{birthYear} was a Leap Year. ");
+            }
+
+            else
+            {
+                Console.WriteLine($"{birthYear} was NOT a Leap Year. ");
+            }
+
+            Console.ReadLine(); */
+
+            //Ternary Operator - kui ainult kaks võimalust
+            /*
+            string answer = (birthYear % 4 == 0) ? "Leap Year" : "Not a Leap Year";
+            Console.WriteLine(answer);
+            Console.ReadLine(); */
+
+
+
+            
+            Console.WriteLine("Choose your poison: Whisky or wine? ");
+            
+            string yourDrink = Console.ReadLine();
+            string choice =  yourDrink == "whisky" ? "Take your whisky" : "Here is your wine";
+            Console.WriteLine(choice);
+            Console.ReadLine();
+
+
+
+
+
+
         }
     }
 }
